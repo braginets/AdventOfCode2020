cf18106764c8b2897c128bbf169a2772d4079ef7
Eugene Braginets <eugene.braginets@nl.abnamro.com>
2020-12-06 07:35:59 +0100

    Day06-1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,41 +16,24 @@ class MainActivity : AppCompatActivity() {
 
     fun readFile(): Array<String> {
         val inputString = application.assets.open("data.txt").bufferedReader().use { it.readText() }
-        val list: List<String> = inputString.split("\n")
-        return list.toTypedArray()
+        return inputString.split("\n\n").toTypedArray()
     }
 
-
-    private fun getRow(data: String): Int {
-        val rowData = data.replace('F', '0').replace('B', '1')
-        val decoded = Integer.parseInt(rowData, 2);
-        return decoded.toInt()
-    }
-
-    private fun getColumn(data: String): Int {
-        val columnData = data.replace('R', '1').replace('L', '0')
-        val decoded = Integer.parseInt(columnData, 2);
-        return decoded.toInt()
-
-    }
     private fun start() {
 
         Log.d("AoC", "Start");
         val data = readFile()
 
-        val ids = data.map {
-                val row = getRow(it.dropLast(3))
-                val column = getColumn(it.takeLast(3))
-                row * 8 + column
-            }
-            .sorted()
+        val answer = data
+            .map { it.split("\n")
+                .map { it.toSet() }
+                .flatMap { it.asIterable() }
+                .toSet()
+                }
+            .flatMap { it.asIterable() }
+            .count()
 
-        var seatID = ids.first()
-        var i: Int = 0
-
-        while (ids[i] == (i + seatID)) { i++ }
-
-        Log.d("AoC - result", "${i + seatID}");
+        Log.d("AoC - result", "${answer}");
     }
 }
 
