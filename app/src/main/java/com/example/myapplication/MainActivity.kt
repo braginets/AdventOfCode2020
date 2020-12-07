681a083a1d00ed7cbfa55a6868c52dc5e7e3401a
Eugene Braginets <eugene.braginets@nl.abnamro.com>
2020-12-07 22:04:11 +0100

    Day07-1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.time.temporal.TemporalAmount
+import java.util.*
+import kotlin.collections.HashMap
+import java.util.NavigableMap
+
+const val bagName = "shiny gold"
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,29 +21,54 @@ class MainActivity : AppCompatActivity() {
 
     fun readFile(): Array<String> {
         val inputString = application.assets.open("data.txt").bufferedReader().use { it.readText() }
-        return inputString.split("\n\n").toTypedArray()
+        return inputString.split("\n").toTypedArray()
     }
+
+    data class Bag(val color: String, val amount: Int)
+
+    fun bagParser(data: Array<String>): Int {
+
+        // key is the innner bag, value is list of all outer bags
+        var reversedBags = HashMap<String, MutableList<Bag>>()
+
+         data.forEach {
+
+             // Learn finally regular expressions, Eugene!!!!
+            val rule = it.split(" bags contain no other bags.", " bags contain ", " bag, ", " bags, ", " bag.", " bags.").dropLast(1)
+            val outer = rule.first()
+            rule.drop(1).forEach {
+                val components = it.split(" ")
+                val amount = components.first().toInt()
+                val bagColor = components.drop(1).joinToString(" ")
+                val bag = Bag(outer, amount)
+
+                val bags: List<Bag> = reversedBags[bagColor] ?: listOf()
+                reversedBags[bagColor] = (bags + listOf(bag)).toMutableList()
+            }
+
+        }
+
+        return  findParents("shiny gold",reversedBags).toSet().count()
+    }
+
+
+    fun findParents(bag: String, data: HashMap<String, MutableList<Bag>>): List<String> {
+
+        val parents = data[bag] ?: return listOf(bag)
+        val chain = parents.map {
+                listOf(it.color) + findParents(it.color, data)
+            }.flatten().toList()
+        return chain
+        }
 
     private fun start() {
 
         Log.d("AoC", "Start");
         val data = readFile()
 
-        val answer = data
-            .map { it.split("\n")
-                .map { it.toSet() }
-                }
-            .map {
-                var set = it.first().toMutableSet()
-                for (a in it) {
-                    set.retainAll(a)
-                }
-                set
-            }
-            .flatMap { it.asIterable() }
-            .count()
+        Log.d("AoC", "${bagParser(data)}");
 
-        Log.d("AoC - result", "${answer}");
+
     }
 }
 
