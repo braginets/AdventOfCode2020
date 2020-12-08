4e8ceda736c22a3b1547bee0e86ce8ac7ee06e01
Eugene Braginets <eugene.braginets@nl.abnamro.com>
2020-12-08 11:06:19 +0100

    Day08

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.time.temporal.TemporalAmount
 import java.util.*
 import kotlin.collections.HashMap
 import java.util.NavigableMap
@@ -26,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     data class Bag(val color: String, val amount: Int)
 
-    fun bagParser(data: Array<String>): Int {
+    fun bagParser(data: List<String>): Int {
 
         // key is the innner bag, value is list of all outer bags
         var bagsStore = HashMap<String, MutableList<Bag>>()
@@ -53,7 +52,6 @@ class MainActivity : AppCompatActivity() {
         return  findBags("shiny gold",1, bagsStore).count() - 1
     }
 
-
     fun findBags(bag: String, amount: Int, data: HashMap<String, MutableList<Bag>>): List<String> {
 
         //how do you multiply elements in collections properly in Kotlin?
@@ -66,9 +64,79 @@ class MainActivity : AppCompatActivity() {
         return Collections.nCopies(amount, bag) + Collections.nCopies(amount, chain).flatten()
         }
 
+    data class Instruction(val operation: String, val argument: String)
+
+    private fun runCode(code: List<Instruction>): Int {
+
+        val c: List<Int> = code.withIndex().filter { it.value.operation == "jmp" || it.value.operation == "nop" }.map { it.index }
+        var result: Int = 0
+        for (it in c) {
+
+            var mutableCode = code.toMutableList()
+
+            val inToReplace = mutableCode[it]
+
+            if (inToReplace.operation == "jmp") {
+                mutableCode[it] = Instruction("nop", inToReplace.argument)
+            } else if (inToReplace.operation == "nop") {
+                mutableCode[it] = Instruction("jmp", inToReplace.argument)
+            }
+
+            var accumulator: Int = 0
+            var offset: Int = 0
+            var offsets = mutableSetOf<Int>()
+            var solved = true
+
+            while (offset != mutableCode.count()) {
+
+                val instruction = mutableCode[offset]
+
+                if (offsets.contains(offset)) {
+                    solved = false
+                    break
+                }
+
+                offsets.add(offset)
+                if (instruction.operation == "acc") {
+                    accumulator += instruction.argument.toInt()
+                    offset += 1
+                } else if (instruction.operation == "jmp") {
+                    offset += instruction.argument.toInt()
+                } else if (instruction.operation == "nop") {
+                    offset += 1
+                }
+
+            }
+
+            if (solved) {
+                result = accumulator
+                break
+            }
+        }
+
+        return result
+    }
+
+
+    private fun problem1(data: List<String>) {
+        Log.d("AoC - data", "${bagParser(data)}");
+    }
+
+    private fun problem2(data: List<String>) {
+        val code = data.map {
+            val instr = it.split(" ")
+            Instruction(instr[0], instr[1])
+        }
+
+        runCode(code)
+    }
+
     private fun start() {
 
-        val data = readFile()
+        val data = readFile().toList()
+
+//        problem1(data)
+        problem2(data)
     }
 }
 
